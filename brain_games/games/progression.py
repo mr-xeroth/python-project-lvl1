51850d9888a99c63df9c9cac57baaa9950396a32
Boris Ageyev <mr.broomy@gmail.com>
2022-01-14 19:51:17 +0500

    progression.py updated

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,24 +3,29 @@
 from random import randrange
 
 
-# returns list ['question', 'solution']
-def exec_game_step():
+def make_sequence():
     seq_len = randrange(6, 11)
     seq_init_term = randrange(2, 31)
     seq_diff = randrange(2, 10)
-    seq_skip = randrange(seq_len)
+    return [seq_init_term + seq_diff * i for i in range(seq_len)]
 
-    seq_list = []
-    next_term = seq_init_term
 
-    for i in range(seq_len):
-        if i != seq_skip:
-            seq_list.append(str(next_term))
-        else:
-            solution_line = str(next_term)
-            seq_list.append('..')
-        next_term += seq_diff
-    sequence_line = ' '.join(seq_list)
+# takes index of term, replaces it w/ '..'
+def cut_term(sequence, term):
+    num = sequence[term]
+    sequence[term] = '..'
+    return(num)
+
+
+# returns list ['question', 'solution']
+def exec_game_step():
+    sequence = make_sequence()
+    num = cut_term(sequence, randrange(len(sequence)))
+
+    solution_line = str(num)
+
+    sequence = [str(i) for i in sequence]
+    sequence_line = ' '.join(sequence)
     return [sequence_line, solution_line]
 
 
